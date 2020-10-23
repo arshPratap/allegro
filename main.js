68b4c2a5a39c78f4dd38dd9ff0ed11d6b805b437
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-10-24 00:33:12 +0530

    setup files

---
+++ main.js
@@ -0,0 +1,23 @@
+const { protocol } = require("electron");
+const electron = require("electron")
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url"); 
+
+let window ;
+
+function renderWindow(){
+    window = new BrowserWindow();
+    window.loadURL(url.format({
+       pathname: path.join(__dirname,"index.html"),
+       protocol: "file",
+       slashes: true
+    }));
+
+    window.on('closed',() => {
+        window = null;
+    })
+}
+
+app.on("ready",renderWindow);
